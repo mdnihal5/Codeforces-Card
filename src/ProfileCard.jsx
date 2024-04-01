07f10c1c912583d3320e821944bb8d14dea86dc3
mdnihal5 <mdnihal.private1@gmail.com>
2024-04-01 15:17:00 +0530

    made some changes

--- src/ProfileCard.jsx
+++ src/ProfileCard.jsx
@@ -30,59 +30,60 @@ const ProfileCard = (User) => {
   const url =
     "https://userpic.codeforces.org/3332594/title/6e3ce0bb53e6ed79.jpg";
   return (
-    <div
-      ref={cardRef}
-      className={`shadow-lg shadow-indigo-500/40 profile-card grid justify-center h-[450px] w-[400px] rounded-2xl mx-auto my-8  bg-${bgColor}`}
-    >
-      <div>
-        <div className="w-[120px] border-solid m-auto  flex items-center jus my-4">
-          <img className="bg-cover rounded-lg" src={url} alt="logo" />
-        </div>
-        <div className=" font-bold grid">
-          <h3 className=" text-cyan-500">{user.tier}</h3>
-          <div className="w-[300px] flex gap-3">
-            <label className="mt-1.5">User Name :</label>
-            <h1 className=" text-cyan-500 text-2xl">{user.username}</h1>
-          </div>
-          <div className="w-[300px] flex gap-3">
-            <label>Contest Rating :</label>
-            <h3 className=" text-cyan-500">{user.contestRating}</h3>
-          </div>
-          <div className="w-[300px] flex gap-3">
-            <label>Max Rating :</label>
-            <h3 className=" text-cyan-500">{user.maxrating}</h3>
-          </div>
-          <div className="w-[300px] flex gap-3">
-            <label>Rank:</label>
-            <h3>{user.rank}</h3>
-          </div>
-          <div className="w-[300px] flex gap-3">
-            <label>Contribution :</label>
-            <h3>{user.contribution}</h3>
-          </div>
-          <div className="w-[300px] flex gap-3">
-            <label>Friends Of :</label>
-            <h3>{user.friendsOf}</h3>
-          </div>
-        </div>
-      </div>
-      <button
-        className="btn btn-sm btn-primary  mx-6"
-        onClick={handleDownloadImage}
+    <div className="w-[100vw] h-[100vh]" ref={cardRef}>
+      <div
+        className={`shadow-lg shadow-indigo-500/60 profile-card grid justify-center h-[450px] w-[400px] rounded-2xl mx-auto my-8  bg-${bgColor}`}
       >
-        Download
-      </button>
-
-      {imageUrl && (
+        <div>
+          <div className="w-[120px] border-solid m-auto  flex items-center jus my-4">
+            <img className="bg-cover rounded-lg" src={url} alt="logo" />
+          </div>
+          <div className=" font-bold grid">
+            <h3 className=" text-cyan-500">{user.tier}</h3>
+            <div className="w-[300px] flex gap-3">
+              <label className="mt-1.5">User Name :</label>
+              <h1 className=" text-cyan-500 text-2xl">{user.username}</h1>
+            </div>
+            <div className="w-[300px] flex gap-3">
+              <label>Contest Rating :</label>
+              <h3 className=" text-cyan-500">{user.contestRating}</h3>
+            </div>
+            <div className="w-[300px] flex gap-3">
+              <label>Max Rating :</label>
+              <h3 className=" text-cyan-500">{user.maxrating}</h3>
+            </div>
+            <div className="w-[300px] flex gap-3">
+              <label>Rank:</label>
+              <h3>{user.rank}</h3>
+            </div>
+            <div className="w-[300px] flex gap-3">
+              <label>Contribution :</label>
+              <h3>{user.contribution}</h3>
+            </div>
+            <div className="w-[300px] flex gap-3">
+              <label>Friends Of :</label>
+              <h3>{user.friendsOf}</h3>
+            </div>
+          </div>
+        </div>
         <button
-          className="btn btn-sm btn-primary"
+          className="btn btn-sm btn-primary  mx-6"
           onClick={handleDownloadImage}
         >
-          <a href={imageUrl} download="profile.png">
-            Download Image
-          </a>
+          Download
         </button>
-      )}
+
+        {imageUrl && (
+          <button
+            className="btn btn-sm btn-primary"
+            onClick={handleDownloadImage}
+          >
+            <a href={imageUrl} download="profile.png">
+              Download Image
+            </a>
+          </button>
+        )}
+      </div>
     </div>
   );
 };
