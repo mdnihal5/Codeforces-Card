e3a57d6c81fe120505a89327539701607ef59456
Md Nihal <111081295+mdnihal5@users.noreply.github.com>
2024-05-02 08:50:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -52,18 +52,6 @@ const App = () => {
         contribution: Data.contribution,
       });
   }, [Data]);
-  $(document).ready(
-      document.getElementById("Input").addEventListener("keypress", function(event) {
-      // If the user presses the "Enter" key on the keyboard
-      if (event.key === "Enter") {
-      // Cancel the default action, if needed
-      event.preventDefault();
-      // Trigger the button element with a click
-      document.getElementById("Button").click();
-    }
-  }); 
-       
-);
   return (
     <div className="App">
       <div className="m-auto flex item-center justify-center">
